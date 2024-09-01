3689bb810d0b1e5e979d086644aa5cd2919e433e
Harshith <harshaharshith2109@gmail.com>
2024-09-01 10:26:20 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import NavBar from "./components/NavBar.js";
+import Hero from "./components/Hero.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Footer from "./components/Footer.js";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <NavBar/>
+   <Hero/>
+   <About/>
+   <Contact/>
+   <Footer/>
+   </>
   );
 }
 
